9c646cc4d440656a5b3fd06898c2175124e90a47
Alba Carmona <albacarmona91@gmail.com>
2017-12-18 16:08:28 +0100

    Hecha la interfaz Dispositivo (Device).

---
+++ ProcesoShneider/ProcesoShneider/Device.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcesoShneider
+{
+    interface Device
+    {
+        // parameters
+
+        int id { get; set; }
+        string serial_number { get; set; } // example: 12AB345CDE
+        string brand { get; set; }
+        string model { get; set; }
+    }
+}
