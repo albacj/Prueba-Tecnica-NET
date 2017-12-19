a04e2181ddbab8721900fa671cdd2d904f90f02b
Alba Carmona <albacarmona91@gmail.com>
2017-12-19 16:19:39 +0100

    Hecha la tabla relacionada con WaterMeter.

---
+++ ProcesoShneider/dbo.WaterMeter.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[WaterMeter]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [SerialNumber] NVARCHAR(50) NOT NULL, 
+    [Brand] NVARCHAR(50) NULL, 
+    [Model] NVARCHAR(50) NULL
+)
