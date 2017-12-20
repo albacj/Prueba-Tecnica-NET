08bcd9de41a9f75821b4960ff8f8d26b6bed7378
Alba Carmona <albacarmona91@gmail.com>
2017-12-20 17:22:49 +0100

    Creada la clase de excepciones para dos dispositivos que son iguales.

---
+++ ProcesoShneider/ProcesoShneider.Tests/DevicesExceptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcesoShneider.Tests
+{
+    class DevicesExceptions : Exception
+    {
+
+        // 2 devices are similar if their serial numbers are similar.
+        public void TwoDevicesEquals(Device d1, Device d2)
+        {
+            if(d1.serial_number == d2.serial_number)
+            {
+                string msg = "This device has been registered.";
+            }
+        }
+
+    }
+}
