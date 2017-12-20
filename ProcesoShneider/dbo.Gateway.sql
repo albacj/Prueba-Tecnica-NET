3eafef088861f297b14c3b35b02a4c58df79661e
Alba Carmona <albacarmona91@gmail.com>
2017-12-20 12:31:27 +0100

    Creadas las tablas WaterAndLightMeter y Gateway.

---
+++ ProcesoShneider/dbo.Gateway.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Gateway]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [SerialNumber] NVARCHAR(50) NOT NULL, 
+    [Brand] NVARCHAR(50) NULL, 
+    [Model] NVARCHAR(50) NULL, 
+    [Ip] NVARCHAR(50) NOT NULL, 
+    [Port] INT NULL
+)
---
+++ ProcesoShneider/dbo.WaterAndLightMeter.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[WaterAndLightMeter]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Serial Number] NVARCHAR(50) NOT NULL, 
+    [Brand] NVARCHAR(50) NULL, 
+    [Model] NVARCHAR(50) NULL
+)
